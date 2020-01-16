8f83b73ba26416cc56536ae52b7ee3b8fc35ced0
homayramussarrat <51522781+homayramussarrat@users.noreply.github.com>
2020-01-15 23:17:56 -0500

    Add files via upload

---
+++ EmailMain.java
@@ -0,0 +1,15 @@
+
+public class EmailMain {
+	
+	public static void main(String[] args) {
+		
+		Email email1 = new Email("John", "Smith");
+	    System.out.println(email1.showInfo());
+	
+		
+				
+		
+		
+	}
+
+}
